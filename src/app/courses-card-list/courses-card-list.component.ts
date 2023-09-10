cfddaa1c9a4a33fc1c6c49cc774086ed34425c26
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-09-10 11:32:41 -0500

    View Layer patterns

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.scss']
+})
+export class CoursesCardListComponent {
+
+  @Input()
+  courses: Course[];
+
+}
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { Course } from "../model/course";
+
+@Injectable({ providedIn: "root" })
+export class CoursesService {
+  constructor(private _http: HttpClient) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this._http.get<Course[]>("/api/courses").pipe(
+      map((response) => response["payload"]),
+      shareReplay()
+    );
+  }
+}
